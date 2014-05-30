4a17fc3443b659c4d67e4b05b07106139c66259a
rhklinux <rhklinux@yahoo.com>
2014-05-30 10:51:32 +0530

    debug.h added

---
+++ debug.h
@@ -0,0 +1,23 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+//
+// Enable/Disable log
+//
+#define DEBUG 1
+
+//
+// specify log file path in quotes.
+// If commented out logs will written to std::cerr
+//
+#define LOG_FILE "test.log"
+
+//
+// Enable/Disable locking
+//
+#define THREAD_SAFE 1
+
+#endif //DEBUG_H
+
+
+
